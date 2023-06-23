20309f9f8b75d72d5cc682848142b0259e08dea9
Rhay <riswanhadi91@gmai.com>
2023-06-23 23:26:04 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/testharian/testarray/TestArray.java
@@ -0,0 +1,13 @@
+package com.juaracoding.testharian.testarray;/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author Rhay Riswan Hadiyanto
+Java Developer
+Created on 23/06/2023 23:24
+@Last Modified 23/06/2023 23:24
+Version 1.0
+*/
+
+public class TestArray {
+
+}
